41e3d888930000524793d59250c664c5a7ac260a
Kamil Kozak <130604@student.uni.opole.pl>
2023-03-10 17:39:23 +0100

    Dodaj pliki projektów.

---
+++ TourDeOpole/TourDeOpole/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using TourDeOpole.ViewModels;
+using Xamarin.Forms;
+
+namespace TourDeOpole.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
