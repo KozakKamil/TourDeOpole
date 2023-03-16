3977495f5da68a9d441a8e3f6f94cef02e69e7c5
KozakKamil <kozak4990@gmail.com>
2023-03-16 12:13:19 +0100

    getting information from JSON file through HTTP request

---
+++ TourDeOpole/TourDeOpole/Services/JSONService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using TourDeOpole.Models;
+
+namespace TourDeOpole.Services
+{
+    internal class JSONService
+    {
+        public async static Task<Location> GetDataAsync(string url)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var response = await httpClient.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    var json = await response.Content.ReadAsStringAsync();
+                    var data = JsonConvert.DeserializeObject<Location>(json);
+                    return data;
+                }
+                else
+                {
+                    throw new Exception($"Request failed with status code {response.StatusCode}");
+                }
+            }
+        }
+    }
+}
--- TourDeOpole/TourDeOpole/Views/MainView.xaml.cs
+++ TourDeOpole/TourDeOpole/Views/MainView.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TourDeOpole.Services;
 using TourDeOpole.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -24,6 +25,17 @@ namespace TourDeOpole.Views
             {
                 _viewModel.getLocation();
             };
+        }
+
+        private async void LoadData()
+        {
+            var data = await JSONService.GetDataAsync("https://raw.githubusercontent.com/KozakKamil/TourDeOpole/master/Data/Data.json");
+            BindingContext = data;
+        }
+        private async void OnGetDataButtonClicked(object sender, EventArgs e)
+        {
+            var data = await JSONService.GetDataAsync("https://raw.githubusercontent.com/KozakKamil/TourDeOpole/master/Data/Data.json");
+            locationLabel.Text = $"Lokalizacja: {data.Name}, {data.Description}";
         }
     }
 }
