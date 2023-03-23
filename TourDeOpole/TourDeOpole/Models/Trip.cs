2e61713be16189f912f2cc8b1faa5f2af41a8d89
KozakKamil <kozak4990@gmail.com>
2023-03-23 11:14:06 +0100

    implemented UI for tripview

--- TourDeOpole/TourDeOpole/Models/Trip.cs
+++ TourDeOpole/TourDeOpole/Models/Trip.cs
@@ -7,9 +7,11 @@ namespace TourDeOpole.Models
     public class Trip
     {
         public int TripID { get; set; }
+
+        public string Image { get; set; }
         public string Name { get; set;}
         public string Description { get; set;}
-        public double Time { get; set;}
+        public string Time { get; set;}
         public bool IsCustom { get; set;}
     }
 }
--- TourDeOpole/TourDeOpole/ViewModels/TripViewModel.cs
+++ TourDeOpole/TourDeOpole/ViewModels/TripViewModel.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 using System.Threading.Tasks;
+using TourDeOpole.Models;
 using TourDeOpole.Services;
 using Xamarin.Forms;
+using Xamarin.Forms.Shapes;
 
 namespace TourDeOpole.ViewModels
 {
@@ -11,11 +14,18 @@ namespace TourDeOpole.ViewModels
     {
         public Command GoToDetailsCommand { get; set; }
         public Command GoToAddCommand { get; set; }
-
-        public TripViewModel() 
+        public ObservableCollection<Trip> myTrip { get; set; }
+        public TripViewModel()
         {
+
             GoToDetailsCommand = new Command(GoToDetails);
             GoToAddCommand = new Command(GoToAddTrip);
+            myTrip = new ObservableCollection<Trip>()
+        {
+            new Trip {Image = "TopImage.jpg", Name = "Trip 1", Time = "1h30m" },
+            new Trip {Image = "TopImage.jpg", Name = "Trip 2", Time = "1h30m" },
+            new Trip {Image = "TopImage.jpg", Name = "Trip 3", Time = "1h30m" }
+        };
         }
 
         private async void GoToDetails()
@@ -27,5 +37,6 @@ namespace TourDeOpole.ViewModels
             await NavigationService.GoToAddTrip();
         }
 
+     
     }
 }
