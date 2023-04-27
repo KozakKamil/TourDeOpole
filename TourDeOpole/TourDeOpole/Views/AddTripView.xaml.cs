b090f89f58fba0fa7fc803ca4d953d5b809d9942
Robert Szczygieł <roszcz17@gmail.com>
2023-04-27 20:25:51 +0200

    added Dodawanie Tras UI

--- TourDeOpole/TourDeOpole/Views/AddTripView.xaml.cs
+++ TourDeOpole/TourDeOpole/Views/AddTripView.xaml.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TourDeOpole.Models;
+using TourDeOpole.Services;
 using TourDeOpole.ViewModels;
+
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -18,6 +21,38 @@ namespace TourDeOpole.Views
             InitializeComponent();
             viewModel= new AddTripViewModel();
             BindingContext= viewModel;
+        }
+
+        private int Count = 0;
+
+        private async void OnButtonClickedRemove(object sender, EventArgs e)
+        {
+           try {
+                AddTripViewLayout.Children.RemoveAt(Count - 1);
+                Count--;
+            } catch (IndexOutOfRangeException ex)
+           {
+
+                await DisplayAlert("nazwa", "wiadomosc", "ok");
+                throw new IndexOutOfRangeException("brak elementow do usuniecia", ex);
+           }
+        }
+                
+            private async void OnButtonClickedAdd(object sender, EventArgs e)
+        {
+            Frame frame = new Frame();
+            frame.Margin =new Thickness (0, 20, 0, 30);
+            frame.CornerRadius = 10;
+            frame.Padding = 0;
+            frame.BorderColor = Color.LightGray;
+            
+            Picker dynamicPicker = new Picker();
+            dynamicPicker.Margin = new Thickness(15, 15, 0, 0);
+            await DisplayAlert("Sukces!", "Dodano nowa trase", "ok");
+            frame.Content = dynamicPicker;
+            AddTripViewLayout.Children.Add(frame);
+            Count++;
+        
         }
 
         //Tutaj piszemy tylko UI, metody do viewModelu
