00c261dff7dbffde3552638a9426f340504db568
KozakKamil <kozak4990@gmail.com>
2023-05-12 10:33:20 +0200

    added new binding

--- TourDeOpole/TourDeOpole/Models/Place.cs
+++ TourDeOpole/TourDeOpole/Models/Place.cs
@@ -18,6 +18,10 @@ namespace TourDeOpole.Models
         public double Longitude { get; set; }
         public bool IsCustom { get; set; }
         public bool IsFavourite { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string Number { get; set; }
+        public string PostalCode { get; set; }
         public string ImageSource => IsFavourite? "favoriteCheck.png" : "favorite.png";
 
 
