1e5544fa7f59df118bd19fcfccadfeb1e558c863
Robert Szczygieł <roszcz17@gmail.com>
2023-06-04 16:28:53 +0200

    Added button "Najblizej ciebie"

--- TourDeOpole/TourDeOpole.Android/Resources/Resource.designer.cs
+++ TourDeOpole/TourDeOpole.Android/Resources/Resource.designer.cs
@@ -21830,31 +21830,34 @@ namespace TourDeOpole.Droid
 			public const int preference_list_divider_material = 2131165340;
 			
 			// aapt resource value: 0x7F07009D
-			public const int Right = 2131165341;
+			public const int QR = 2131165341;
 			
 			// aapt resource value: 0x7F07009E
-			public const int Search = 2131165342;
+			public const int Right = 2131165342;
 			
 			// aapt resource value: 0x7F07009F
-			public const int settings = 2131165343;
+			public const int Search = 2131165343;
 			
 			// aapt resource value: 0x7F0700A0
-			public const int test_custom_background = 2131165344;
+			public const int settings = 2131165344;
 			
 			// aapt resource value: 0x7F0700A1
-			public const int tooltip_frame_dark = 2131165345;
+			public const int test_custom_background = 2131165345;
 			
 			// aapt resource value: 0x7F0700A2
-			public const int tooltip_frame_light = 2131165346;
+			public const int tooltip_frame_dark = 2131165346;
 			
 			// aapt resource value: 0x7F0700A3
-			public const int TopImage = 2131165347;
+			public const int tooltip_frame_light = 2131165347;
 			
 			// aapt resource value: 0x7F0700A4
-			public const int xamarin_logo = 2131165348;
+			public const int TopImage = 2131165348;
 			
 			// aapt resource value: 0x7F0700A5
-			public const int XMLCursor = 2131165349;
+			public const int xamarin_logo = 2131165349;
+			
+			// aapt resource value: 0x7F0700A6
+			public const int XMLCursor = 2131165350;
 			
 			static Drawable()
 			{
